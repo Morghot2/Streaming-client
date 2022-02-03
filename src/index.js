2eaa7468912c54fe321039e86032758be62c724c
Morghot2 <maximilian.jaron@gmail.com>
2022-02-03 20:29:12 +0100

    Checking GIT behaviour

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./components/App";
-
+// Checking git behaviour
 ReactDOM.render(<App />, document.querySelector("#root"));
