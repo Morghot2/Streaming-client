05d2a68cea609139cd604f208526565322588b66
Morghot2 <maximilian.jaron@gmail.com>
2022-02-03 21:15:03 +0100

    Navigating with React Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,22 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 const PageOne = () => {
-  return <div>PageOne</div>;
+  return (
+    <div>
+      PageOne<Link to="/pagetwo">Navigate to Page 2</Link>
+    </div>
+  );
 };
 const PageTwo = () => {
   return (
     <div>
       PageTwo<button>Clicke me</button>
+      <Link to="/">Navigate to Page 1</Link>
     </div>
   );
 };
-
+//fdsf
 const App = () => {
   return (
     <div>
