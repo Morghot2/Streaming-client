db1ed4efeba19c97d29c07d19a993446cd1c15c3
Morghot2 <maximilian.jaron@gmail.com>
2022-02-04 17:38:32 +0100

    Building the Auth Reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,4 @@
 import { combineReducers } from "redux";
+import authReducer from "./authReducer";
 
-export default combineReducers({ replaceMe: () => 3})
+export default combineReducers({ auth: authReducer });
