ac3057114eb6752da538b76d56c25589f2ca0217
Morghot2 <maximilian.jaron@gmail.com>
2022-02-05 21:34:11 +0100

    Creating Streams Through Action Creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,22 @@
+import streams from "../apis/streams";
 import { SIGN_IN, SIGN_OUT } from "./types";
 
 export const signIn = (userId) => {
   return {
     type: SIGN_IN,
-    payload: userId 
+    payload: userId,
   };
 };
 
 export const signOut = (userId) => {
   return {
     type: SIGN_OUT,
-    payload: userId
+    payload: userId,
+  };
+};
+
+export const createStream = (formValues) => {
+  return async (dispatch) => {
+    streams.post("/streams", formValues);
   };
 };
--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
 import formValues from "redux-form/lib/formValues";
+import { connect } from 'react-redux'
+import { createStream } from "../../actions";
 class StreamCreate extends React.Component {
   renderError({ error, touched }) {
     if (touched && error) {
