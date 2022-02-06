db390b7e335c8b179437ad716e2a85d77bc60405
Morghot2 <maximilian.jaron@gmail.com>
2022-02-07 00:47:44 +0100

    Rendering All Streams

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,16 +1,33 @@
-import React from 'react';
-import { connect } from 'react-redux'
-import { fetchStreams } from '../../actions';
+import React from "react";
+import { connect } from "react-redux";
+import { fetchStreams } from "../../actions";
 
 class StreamList extends React.Component {
   componentDidMount() {
-    this.props.fetchStreams()
+    this.props.fetchStreams();
+  }
+  renderList() {
+    return this.props.streams.map((stream) => {
+      return (
+        <div className="item" key={stream.id}>
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
   }
   render() {
-    return <div>Streams List</div>
+    return (
+      <div><h2>Streams</h2><div className="ui celled list">{this.renderList()}</div></div>
+    )
   }
 }
-  
 
+const mapStateToProps = (state) => {
+  return { streams: Object.values(state.streams) };
+};
 
-export default connect(null, { fetchStreams })(StreamList);
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
