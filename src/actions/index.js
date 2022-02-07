6f8506e51877c53630debc6f3f3a3d1422f31d1e
Morghot2 <maximilian.jaron@gmail.com>
2022-02-07 19:16:04 +0100

    Implementing Programmatic Navigation

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import { formValues } from "redux-form";
+import history from "../history";
 import streams from "../apis/streams";
 import {
   SIGN_IN,
@@ -26,8 +27,9 @@ export const signOut = (userId) => {
 
 export const createStream = (formValues) => async (dispatch, getState) => {
   const { userId } = getState().auth;
-  const response = await streams.post("/streams", {...formValues, userId});
+  const response = await streams.post("/streams", { ...formValues, userId });
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  history.push('/')
 };
 export const fetchStreams = () => async (dispatch, getState) => {
   const response = await streams.get("/streams");
