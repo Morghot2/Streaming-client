cbda208063d9566cce50e7c68536abdeb45fa940
Morghot2 <maximilian.jaron@gmail.com>
2022-02-07 17:26:05 +0100

    Conditionally Showing Edit and Delete

--- src/actions/index.js
+++ src/actions/index.js
@@ -24,16 +24,13 @@ export const signOut = (userId) => {
   };
 };
 
-export const createStream = (formValues) => {
-  return async (dispatch) => {
-    const response = await streams.post("/streams", formValues);
-    dispatch({ type: CREATE_STREAM, payload: response.data });
-  };
+export const createStream = (formValues) => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+  const response = await streams.post("/streams", {...formValues, userId});
+  dispatch({ type: CREATE_STREAM, payload: response.data });
 };
-
 export const fetchStreams = () => async (dispatch, getState) => {
-  const { userId } = getState().auth
-  const response = await streams.get("/streams", { ...formValues, userId});
+  const response = await streams.get("/streams");
   dispatch({ type: FETCH_STREAMS, payload: response.data });
 };
 
